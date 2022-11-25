b4a0205b6b9d351671bf7c4bfff625acc3facf09
carlos.prado <carlos.prado@softwareestrategico.com>
2022-11-25 13:45:04 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Table from './components/table'
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
-function App() {
+const  App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ backgroundColor: '#D4D4D4', p:6}}>
+      <Grid container justifyContent="center">
+        <Grid item xs={12}>
+          <Table></Table>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
